8c1d3de42250bf960a2bb77201ad4885c140bd2f
WarniWarni <szymon.halaszuk@gmail.com>
2019-05-22 18:42:12 +0200

    Rozbudowanie logiki postaci, szkieletory mogą umżeć. Dodanie ataku szkieletorów oraz ataku bohatera

---
+++ Kelvz/Assets/Scripts/Enemies/skeleton_action.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skeleton_action : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool _wasTriggered;
+    //
+    public int health = 100;
+    public int attack_strength = 10;
+    //
+
+    private Animator _animator;
+    private GameObject self;
+
+    void Start()
+    {
+        // localScale = transform.localScale;
+        _animator = GetComponent<Animator>();
+        
+//        StartCoroutine(Idle());
+    }
+
+    private void Destroy(){
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Troop")){
+            _animator.SetTrigger("attack");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // jeśli nie koliduje, to idzie w lewo, jeśli koliduje to:
+        if (other.gameObject.CompareTag("Troop"))
+        {
+            _animator.SetTrigger("attack");
+        }
+        // wypadałoby dać tak, żeby odbierać informacje o \
+        // położeniu flagi i w razie co zrobić flip i wojo wraca
+
+        if (other.gameObject.CompareTag("Creatures"))
+        {
+           // jeśli wróg, to nakurwiaj
+           _animator.SetTrigger("attack");
+           // tu wyślij sygnał odbieranko życia
+        }
+
+        if (other.gameObject.CompareTag("Building"))
+        {
+           // jeśli wróg, to nakurwiaj
+           _animator.SetTrigger("attack");
+           // tu wyślij sygnał odbieranko życia
+        }
+        
+    }
+}
---
+++ Kelvz/Assets/Scripts/Troops/Hero_Action.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero_Action : MonoBehaviour
+{
+    private bool _wasTriggered;
+    //
+    public int health = 100;
+    public int attack_strength = 10;
+    //
+
+    private Animator _animator;
+
+    void Start()
+    {
+        // localScale = transform.localScale;
+        _animator = GetComponent<Animator>();
+//        StartCoroutine(Idle());
+    }
+    void make_damage(){
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // jeśli nie koliduje, to idzie w prawo, jeśli koliduje to:
+        if (other.gameObject.CompareTag("Flag"))
+        {
+            _animator.SetTrigger("hero_idle");
+        }
+        // wypadałoby dać tak, żeby odbierać informacje o \
+        // położeniu flagi i w razie co zrobić flip i wojo wraca
+
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+           // jeśli wróg, to nakurwiaj
+           _animator.SetTrigger("hero_attack");
+           // tu wyślij sygnał odbieranko życia
+        }
+    }
+        private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy")){
+            _animator.SetTrigger("hero_attack");
+        }
+        if (other.gameObject.CompareTag("Flag"))
+        {
+            _animator.SetTrigger("hero_idle");
+        }
+    }
+
+}
